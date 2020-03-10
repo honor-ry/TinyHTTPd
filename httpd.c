f0795b8ae53c1fc71f63107b4e1ebd75e23cfd52
MAHuiming <33728226+xiaoma101017@users.noreply.github.com>
2020-03-10 20:19:37 +0800

    整理代码缩进，注释 serve_file，execute_cgi，accept_request 函数

--- httpd.c
+++ httpd.c
@@ -87,47 +87,46 @@ void *accept_request(void *client)
     while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < sizeof(buf))) {
         url[i] = buf[j];
         i++;
-	j++;
+		j++;
     }
     url[i] = '\0';
 
     if (strcasecmp(method, "GET") == 0) {
         query_string = url;
-	while ((*query_string != '?') && (*query_string != '\0')) {
+		while ((*query_string != '?') && (*query_string != '\0')) {
             query_string++;
-	}
-	if (*query_string == '?') {
+		}
+		if (*query_string == '?') {
             cgi = 1;
             *query_string = '\0';
             query_string++;
-	}
+		}
     }
     sprintf(path, "htdocs%s", url);
     if (path[strlen(path) - 1] == '/') {
-	strcat(path, "index.html");
+		strcat(path, "index.html");
     }
     if (stat(path, &st) == -1) {
-	while ((numchars > 0) && strcmp("\n", buf)) {  /* read & discard headers */
-	    numchars = get_line(*((int *)client), buf, sizeof(buf));
-	}
-	not_found(*((int *)client));
+		while ((numchars > 0) && strcmp("\n", buf)) {  /* read & discard headers */
+	    	numchars = get_line(*((int *)client), buf, sizeof(buf));
+		}
+		not_found(*((int *)client));
     }
     else {
-	if ((st.st_mode & S_IFMT) == S_IFDIR) {
+		if ((st.st_mode & S_IFMT) == S_IFDIR) {
             strcat(path, "/index.html");
-	}
-		
-	if ((st.st_mode & S_IXUSR) || (st.st_mode & S_IXGRP) || (st.st_mode & S_IXOTH)) {
-	    cgi = 1;
-	}
-	if (!cgi) {
-	    serve_file(*((int *)client), path);
-	}
-	else {
-            execute_cgi(*((int *)client), path, method, query_string);
-	}
+		}
+		// 如果有执行权限，则认为是 .cgi 脚本
+		if ((st.st_mode & S_IXUSR) || (st.st_mode & S_IXGRP) || (st.st_mode & S_IXOTH)) {
+	    	cgi = 1;
+		}
+		if (!cgi) {
+	    	serve_file(*((int *)client), path); 	// 发送 htdocs/index.html
+		}
+		else {
+            execute_cgi(*((int *)client), path, method, query_string); 	// 执行 CGI 脚本
+		}
     }
-
     close(*((int *)client));
     return NULL;
 }
@@ -219,22 +218,22 @@ void execute_cgi(int client, const char *path,
     buf[0] = 'A'; buf[1] = '\0';
     if (strcasecmp(method, "GET") == 0) {
         while ((numchars > 0) && strcmp("\n", buf)) {  /* read & discard headers */
-	    numchars = get_line(client, buf, sizeof(buf));
-	}
+	    	numchars = get_line(client, buf, sizeof(buf));
+		}
     }
     else {    /* POST */
         numchars = get_line(client, buf, sizeof(buf));
-	while ((numchars > 0) && strcmp("\n", buf)) {
-	    buf[15] = '\0';
-	    if (strcasecmp(buf, "Content-Length:") == 0) {
-		    content_length = atoi(&(buf[16]));
-	    }
-	    numchars = get_line(client, buf, sizeof(buf));
-	}
-	if (content_length == -1) {
-	    bad_request(client);
-	    return;
-	}
+		while ((numchars > 0) && strcmp("\n", buf)) {
+	    	buf[15] = '\0';
+	    	if (strcasecmp(buf, "Content-Length:") == 0) {
+		    	content_length = atoi(&(buf[16]));
+	    	}
+	    	numchars = get_line(client, buf, sizeof(buf));
+		}
+		if (content_length == -1) {
+	    	bad_request(client);
+	    	return;
+		}
     }
 
     sprintf(buf, "HTTP/1.0 200 OK\r\n");
@@ -242,55 +241,56 @@ void execute_cgi(int client, const char *path,
 
     if (pipe(cgi_output) < 0) {
         cannot_execute(client);
-	return;
+		return;
     }
     if (pipe(cgi_input) < 0) {
-	cannot_execute(client);
-	return;
+		cannot_execute(client);
+		return;
     }
 
     if ( (pid = fork()) < 0 ) {
         cannot_execute(client);
-	return;
+		return;
     }
     // 子进程执行 CGI 脚本
     if (pid == 0) {  /* child: CGI script */
         char meth_env[255];
-	char query_env[255];
-	char length_env[255];
+		char query_env[255];
+		char length_env[255];
 
-	dup2(cgi_output[1], 1);
-	dup2(cgi_input[0], 0);
-	close(cgi_output[0]);
-	close(cgi_input[1]);
-	sprintf(meth_env, "REQUEST_METHOD=%s", method);
-	putenv(meth_env);
-	if (strcasecmp(method, "GET") == 0) {
-	    sprintf(query_env, "QUERY_STRING=%s", query_string);
-	    putenv(query_env);
-	}
-	else {   /* POST */
-	    sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
-	    putenv(length_env);
-	}
-	execl(path, path, NULL);
-	exit(0);
+		dup2(cgi_output[1], 1); 	// 子进程标准输出定向到 cgi_output[1]
+		dup2(cgi_input[0], 0); 		// 子进程标准输入定向到 cgi_input[0]
+		close(cgi_output[0]);
+		close(cgi_input[1]);
+		sprintf(meth_env, "REQUEST_METHOD=%s", method);
+		putenv(meth_env);
+		if (strcasecmp(method, "GET") == 0) {
+	    	sprintf(query_env, "QUERY_STRING=%s", query_string);
+	    	putenv(query_env);
+		}
+		else {   /* POST */
+	    	sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
+	    	putenv(length_env);
+		}
+		execl(path, path, NULL);	// 子进程执行 CGI 脚本
+		exit(0);
     } 
     else {    /* parent */
         close(cgi_output[1]);
         close(cgi_input[0]);
-	if (strcasecmp(method, "POST") == 0) {
-	    for (i = 0; i < content_length; i++) {
-	        recv(client, &c, 1, 0);
-		write(cgi_input[1], &c, 1);
-	    }
-	    while (read(cgi_output[0], &c, 1) > 0) {
-		send(client, &c, 1, 0);
-	    }
-	}
-	close(cgi_output[0]);
-	close(cgi_input[1]);
-	waitpid(pid, &status, 0);
+		if (strcasecmp(method, "POST") == 0) {
+	    	for (i = 0; i < content_length; i++) {
+	        	recv(client, &c, 1, 0);
+				write(cgi_input[1], &c, 1);
+	    	}
+			// 从管道 cgi_output[0] 读到子进程处理后的信息，发送给客户端
+	    	while (read(cgi_output[0], &c, 1) > 0) {
+				send(client, &c, 1, 0);
+	    	}
+		}
+		close(cgi_output[0]);
+		close(cgi_input[1]);
+		waitpid(pid, &status, 0);
     }
 }
 
@@ -403,8 +403,8 @@ void serve_file(int client, const char *filename)
     if (resource == NULL)
         not_found(client);
     else {
-        headers(client, filename);
-        cat(client, resource);
+        headers(client, filename);	// 发送 HTTP 响应头信息
+        cat(client, resource);		// 发送 index.html 文件内容给客户端
     }
     fclose(resource);
 }
